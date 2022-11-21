d425fcc2da53a4ad1081ee65478a6aa7c0902f7c
MarufAbrar <maracode1@gmail.com>
2022-11-21 22:27:57 +0300

    alxcheck

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,7 +59,7 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """delete obj from __objects if it’s inside"""
+        """delete obj from __objects if its inside"""
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
@@ -68,3 +68,19 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        '''retrieves an object of type cls with the passed id
+        or none if not found'''
+        if cls is None:
+            return None
+        objs = self.all(cls)
+        for obj in objs.values():
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        '''counts ho many objects of type cls in storage or
+        counts all objects of all classes if cls is None'''
+        return len(self.all(cls))
